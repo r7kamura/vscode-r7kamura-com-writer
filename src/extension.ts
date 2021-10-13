c8fe8b7b7f1da48bae6c4a5ade15debd3bdfa1ca
r7kamura <r7kamura@gmail.com>
2021-10-14 03:10:46 +0900

    Rename name to slug, and fix no ./articles dir case

--- src/extension.ts
+++ src/extension.ts
@@ -11,31 +11,48 @@ export function activate(context: vscode.ExtensionContext) {
     const workspacePath = vscode.workspace.workspaceFolders[0].uri.toString().split(':')[1];
 
 		vscode.window.showInputBox({
-      title: 'name-in-url',
-    }).then((name) => {
+      title: 'Slug (chain-cased-name in URL)',
+    }).then((slug) => {
+      if (!slug) {
+        return;
+      }
+
       vscode.window.showInputBox({
         title: 'Title',
       }).then((title) => {
+        if (!title) {
+          return;
+        }
+
         const date = new Date();
         const yyyy = date.getFullYear();
         const mm = `0${date.getMonth() + 1}`.slice(-2);
         const dd = `0${date.getDate()}`.slice(-2);
-        const filePath = path.join(workspacePath, 'articles', `${yyyy}-${mm}-${dd}-${name}.md`);
+        const articlesPath = path.join(workspacePath, 'articles');
+        const filePath = path.join(workspacePath, 'articles', `${yyyy}-${mm}-${dd}-${slug}.md`);
         const content = `---\ntitle: ${title}\n---\n\n`;
-        fs.writeFile(filePath, content, (error) => {
-          if (error) {
-            return vscode.window.showErrorMessage(`Failed to create ${filePath}`);
+        fs.mkdir(articlesPath, { recursive: true }, (mkdirError) => {
+          if (mkdirError) {
+            vscode.window.showErrorMessage(`Failed to create ${articlesPath}`);
+            return;
           }
-          vscode.window.showInformationMessage(`Created ${filePath}`);
 
-          const vscodeUri = vscode.Uri.file(filePath);
-          vscode.workspace.openTextDocument(vscodeUri).then(vscodeTextDocument => {
-            vscode.window.showTextDocument(vscodeTextDocument).then(editor => {
-              const position = new vscode.Position(5, 1);
-              editor.selections = [new vscode.Selection(position, position)];
-              editor.revealRange(new vscode.Range(position, position));
-            });
-          })
+          fs.writeFile(filePath, content, (error) => {
+            if (error) {
+              vscode.window.showErrorMessage(`Failed to create ${filePath}`);
+              return;
+            }
+            vscode.window.showInformationMessage(`Created ${filePath}`);
+
+            const vscodeUri = vscode.Uri.file(filePath);
+            vscode.workspace.openTextDocument(vscodeUri).then(vscodeTextDocument => {
+              vscode.window.showTextDocument(vscodeTextDocument).then(editor => {
+                const position = new vscode.Position(5, 1);
+                editor.selections = [new vscode.Selection(position, position)];
+                editor.revealRange(new vscode.Range(position, position));
+              });
+            })
+          });
         });
       });
     });
