356190091fa93ab021b165257b989a031307ad65
Ryo Nakamura <r7kamura@gmail.com>
2022-01-18 02:47:41 +0900

    Simplify command behavior so that it automatically create new diary

--- src/extension.ts
+++ src/extension.ts
@@ -16,72 +16,53 @@ export function activate(context: vscode.ExtensionContext) {
         .toString()
         .split(":")[1];
 
-      vscode.window
-        .showInputBox({
-          title: "Slug (chain-cased-name in URL)",
-        })
-        .then((slug) => {
-          if (!slug) {
+      const slug = "diary";
+      const title = "日記";
+      const date = new Date();
+      const yyyy = date.getFullYear();
+      const mm = `0${date.getMonth() + 1}`.slice(-2);
+      const dd = `0${date.getDate()}`.slice(-2);
+      const articlesPath = path.join(workspacePath, "articles");
+      const filePath = path.join(
+        workspacePath,
+        "articles",
+        `${yyyy}-${mm}-${dd}-${slug}.md`
+      );
+
+      if (fs.existsSync(filePath)) {
+        return;
+      }
+
+      const content = `---\ntitle: ${title}\n---\n\n`;
+      fs.mkdir(articlesPath, { recursive: true }, (mkdirError) => {
+        if (mkdirError) {
+          vscode.window.showErrorMessage(`Failed to create ${articlesPath}`);
+          return;
+        }
+
+        fs.writeFile(filePath, content, (error) => {
+          if (error) {
+            vscode.window.showErrorMessage(`Failed to create ${filePath}`);
             return;
           }
+          vscode.window.showInformationMessage(`Created ${filePath}`);
 
-          vscode.window
-            .showInputBox({
-              title: "Title",
-            })
-            .then((title) => {
-              if (!title) {
-                return;
-              }
-
-              const date = new Date();
-              const yyyy = date.getFullYear();
-              const mm = `0${date.getMonth() + 1}`.slice(-2);
-              const dd = `0${date.getDate()}`.slice(-2);
-              const articlesPath = path.join(workspacePath, "articles");
-              const filePath = path.join(
-                workspacePath,
-                "articles",
-                `${yyyy}-${mm}-${dd}-${slug}.md`
-              );
-              const content = `---\ntitle: ${title}\n---\n\n`;
-              fs.mkdir(articlesPath, { recursive: true }, (mkdirError) => {
-                if (mkdirError) {
-                  vscode.window.showErrorMessage(
-                    `Failed to create ${articlesPath}`
-                  );
-                  return;
-                }
-
-                fs.writeFile(filePath, content, (error) => {
-                  if (error) {
-                    vscode.window.showErrorMessage(
-                      `Failed to create ${filePath}`
-                    );
-                    return;
-                  }
-                  vscode.window.showInformationMessage(`Created ${filePath}`);
-
-                  const vscodeUri = vscode.Uri.file(filePath);
-                  vscode.workspace
-                    .openTextDocument(vscodeUri)
-                    .then((vscodeTextDocument) => {
-                      vscode.window
-                        .showTextDocument(vscodeTextDocument)
-                        .then((editor) => {
-                          const position = new vscode.Position(5, 1);
-                          editor.selections = [
-                            new vscode.Selection(position, position),
-                          ];
-                          editor.revealRange(
-                            new vscode.Range(position, position)
-                          );
-                        });
-                    });
+          const vscodeUri = vscode.Uri.file(filePath);
+          vscode.workspace
+            .openTextDocument(vscodeUri)
+            .then((vscodeTextDocument) => {
+              vscode.window
+                .showTextDocument(vscodeTextDocument)
+                .then((editor) => {
+                  const position = new vscode.Position(5, 1);
+                  editor.selections = [
+                    new vscode.Selection(position, position),
+                  ];
+                  editor.revealRange(new vscode.Range(position, position));
                 });
-              });
             });
         });
+      });
     }
   );
 
